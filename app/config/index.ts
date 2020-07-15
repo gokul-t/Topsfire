577621bd3f9172d2a1d4ce1e60ac4aa5563bec1f
Gokul T <gokultmail@gmail.com>
2020-07-15 20:57:04 +0530

    Icon Url placeholder added.

--- app/config/index.ts
+++ app/config/index.ts
@@ -27,7 +27,7 @@ export default {
       url: env.API_URL,
     },
   ],
-  adUnitID: {
+  adUnitID: env.adUnitID || {
     banner: "ca-app-pub-3940256099942544/6300978111",
     interstitial: "ca-app-pub-3940256099942544/1033173712",
     interstitialVideo: "ca-app-pub-3940256099942544/8691691433",
@@ -36,5 +36,5 @@ export default {
     nativeAdvancedVideo: "ca-app-pub-3940256099942544/1044960115",
   },
   dateFormat: "MMM d, Y",
-  iconUri: "http://www.topsfire.com/wp-content/uploads/2018/08/cropped-logo.png",
+  iconUri: env.ICON_URI || "https://via.placeholder.com/100",
 }
